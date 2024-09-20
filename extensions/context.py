b6fa206f51179727f45bac06fffce8502f976d63
Nate Scherer <376408+natescherer@users.noreply.github.com>
2024-09-20 18:55:07 +0000

    fix: fix context updater extension

--- extensions/context.py
+++ extensions/context.py
@@ -7,4 +7,4 @@ class ContextUpdater(ContextHook):
     def hook(self, context):
         # Doing the below ensures that repo actions are only taken on the
         # first, interactive run of the template, not on subsequent updates
-        context["_copier_answers"]["repo_actions"] = "None"
+        del context["_copier_answers"]["repo_actions"]
